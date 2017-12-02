e2738ca8832990e311a0d8732e1f67d4b593cf6e
Tomasz Jonak <tomasz.jonak@forcepoint.com>
2017-12-02 11:02:43 +0100

    Pnet lib example

--- nmap/src/main.rs
+++ nmap/src/main.rs
@@ -1,3 +1,12 @@
+// extern crate libc;
+extern crate pnet;
+
+use pnet::datalink;
+
 fn main() {
-    println!("Hello, world!");
+    // let socket = unsafe { libc::socket(libc::AF_INET, libc::SOCK_RAW, libc::IPPROTO_ICMP); };
+    let interfaces = datalink::interfaces();
+    for interface in interfaces.into_iter() {
+        println!("{}", interface.name);
+    }
 }
