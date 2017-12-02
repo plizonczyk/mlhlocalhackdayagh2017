6a4a57ccb6387af63a34e705394c9be7f2f95490
Piotr Lizonczyk <plizonczyk.public@gmail.com>
2017-12-02 13:37:31 +0100

    Reading CLI arguments

--- nmap/src/main.rs
+++ nmap/src/main.rs
@@ -1,12 +1,36 @@
-// extern crate libc;
 extern crate pnet;
 
-use pnet::datalink;
+use pnet::datalink::{self, interfaces, NetworkInterface};
+use pnet::datalink::Channel::Ethernet;
+
+use std::process;
 
 fn main() {
-    // let socket = unsafe { libc::socket(libc::AF_INET, libc::SOCK_RAW, libc::IPPROTO_ICMP); };
-    let interfaces = datalink::interfaces();
-    for interface in interfaces.into_iter() {
-        println!("{}", interface.name);
-    }
+    let given_iface = match std::env::args().nth(1) {
+        Some(n) => n,
+        None => {
+            println!("Usage: cargo run <interface> <ip>");
+            process::exit(1);
+        }
+    };
+
+    let ip_to_scan = match std::env::args().nth(2) {
+        Some(n) => n,
+        None => {
+            println!("Usage: cargo run <interface> <ip>");
+            process::exit(1);
+        }
+    };
+
+    let cmp_fn = |interface: &NetworkInterface| interface.name == given_iface;
+    let iface = match interfaces().into_iter().filter(cmp_fn).next() {
+        Some(n) => n,
+        None => {
+            println!("Available interfaces:");
+            for interface in interfaces().into_iter() {
+                println!("{}", interface.name);
+            }
+            process::exit(1);
+        }
+    };
 }
